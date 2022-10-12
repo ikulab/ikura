bbd20c8fdaecc90d316f9c1aefc5b8d84689b5fb
caffeine <kouta8145963@gmail.com>
2022-10-12 15:50:40 +0900

    move files from ikulab-motion-viewer

---
+++ src/lib/ikura/shape/shapes.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "./shape.hpp"
+
+#include "./debug/directionDebugObject.hpp"
+
+#include "./bone/bone.hpp"
+#include "./bone/octahedronBone.hpp"
+#include "./bone/stickTetrahedronBone.hpp"
+
+#include "./sphere/sphere.hpp"
+
+#include "./cube/cube.hpp"
+#include "./cube/gradationColorCube.hpp"
+#include "./cube/separatedColorCube.hpp"
+#include "./cube/singleColorCube.hpp"
+
+#include "./floor/filledFloor.hpp"
+#include "./floor/floor.hpp"
+#include "./floor/gridFloor.hpp"
